09278d30721b13bd01f89c019797d1a988901c28
Doradas <45564976+Doradas0@users.noreply.github.com>
2022-06-03 11:18:16 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Example } from "./features/Example";
+
+const App = () => {
+	return (
+		<>
+			<h1>React Template</h1>
+			<Example />
+			<p>Created by Doradas</p>
+			<p>Inspired by https://github.com/alan2207/bulletproof-react </p>
+		</>
+	);
+};
+
+export default App;
