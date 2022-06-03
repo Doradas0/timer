65769edf532c6883cb930f99a92a7e7bc8254b03
root <root@Doradas-Pro7.localdomain>
2022-06-03 11:44:11 +0100

    add unstyled timer
    includes Days, Hours, Minutes, Seconds
    could look to extract out different measurements to seperate components

--- src/features/Timer/Timer.tsx
+++ src/features/Timer/Timer.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 export const Timer = () => {
 	const [timeNow, setTimeNow] = useState(new Date());
+
 	useEffect(() => {
 		const timer = setTimeout(() => {
 			setTimeNow(new Date());
@@ -8,5 +9,25 @@ export const Timer = () => {
 		return () => clearTimeout(timer);
 	}, [timeNow]);
 
-	return <p>{timeNow.toUTCString()}</p>;
+	const TARGET_DATE = new Date("01-17-2027");
+
+	const timeDifference = getTimeBetween(TARGET_DATE, timeNow);
+
+	return(
+		<div>
+			<p>Days: {getDays(timeDifference)}</p>
+			<p>Hours: {getHours(timeDifference)}</p>
+			<p>Minutes: {getMinutes(timeDifference)}</p>
+			<p>Seconds: {getSeconds(timeDifference)}</p>
+		</div>
+	)
 };
+
+const getTimeBetween = (date1: any, date2: any) => {
+	return date1.getTime() - date2.getTime();
+};
+
+const getDays = (time: number) => Math.floor(time / 86400000);
+const getHours = (time: number) => Math.floor(time / 3600000);
+const getMinutes = (time: number) => Math.floor(time / 60000);
+const getSeconds = (time: number) => Math.floor(time / 1000);
